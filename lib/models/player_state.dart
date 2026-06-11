c861467f16574103eb1aa3ed5a433b59543f1f68
Matthieu Sublet <msublet@gmail.com>
2026-06-11 23:46:45 +0200

    Update player_state.dart

--- lib/models/player_state.dart
+++ lib/models/player_state.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:math'; // Nécessaire pour générer des nombres aléatoires (les dés)
+import 'dart:math';
 
 class PlayerState extends ChangeNotifier {
   int pointsDeVie = 20;
   int pointsDeVieMax = 20;
   List<String> inventaire = ['Excalibur Junior'];
   
-  // Nouveaux statuts basés sur les règles
   bool estEvanoui = false;
   bool estMort = false;
 
-  // Le générateur d'aléatoire pour nos lancers de dés
   final Random _random = Random();
 
   // --- GESTION DE LA SANTÉ ET DE L'INVENTAIRE ---
@@ -22,17 +20,14 @@ class PlayerState extends ChangeNotifier {
       pointsDeVie = pointsDeVieMax;
     }
     
-    // Application stricte des règles de la Quête du Graal :
     if (pointsDeVie <= 0) {
       pointsDeVie = 0;
       estMort = true;
-      estEvanoui = true; // S'il est mort, il est techniquement évanoui aussi
+      estEvanoui = true;
     } else if (pointsDeVie <= 5) {
-      // À 5 PV ou moins, Pip s'évanouit
       estEvanoui = true;
       estMort = false;
     } else {
-      // Tout va bien
       estEvanoui = false;
       estMort = false;
     }
@@ -57,15 +52,47 @@ class PlayerState extends ChangeNotifier {
 
   // --- MOTEUR DE RÈGLES ET DE COMBAT ---
 
-  /// Simule le lancer d'un certain nombre de dés à 6 faces
   int lancerDes(int nombreDeDes) {
     int total = 0;
     for (int i = 0; i < nombreDeDes; i++) {
-      total += _random.nextInt(6) + 1; // Génère un nombre entre 1 et 6
+      total += _random.nextInt(6) + 1;
     }
     return total;
   }
 
-  /// Calcule les dommages infligés par une attaque
-  /// Par défaut : il faut faire plus de 6, et les dommages = (Jet - 6)
-  int calculerDommages(int jetDeDes, {int seuilTouche = 6, int degatsFixes = 0
+  int calculerDommages(int jetDeDes, {int seuilTouche = 6, int degatsFixes = 0, int bonusDommage = 0}) {
+    if (degatsFixes > 0) {
+      return degatsFixes;
+    }
+
+    if (jetDeDes > seuilTouche) {
+      return (jetDeDes - seuilTouche) + bonusDommage;
+    }
+    
+    return 0;
+  }
+
+  Map<String, dynamic> attaqueDePip() {
+    int jet = lancerDes(2);
+    int seuil = 6;
+    int bonus = 0;
+
+    String armeUtilisee = 'Mains nues';
+    if (possedeObjet('Excalibur Junior')) {
+      armeUtilisee = 'Excalibur Junior';
+      bonus = 5; 
+    }
+
+    int dommages = calculerDommages(jet, seuilTouche: seuil, bonusDommage: bonus);
+
+    return {
+      'jet': jet,
+      'touche': dommages > 0,
+      'dommages': dommages,
+      'arme': armeUtilisee,
+      'message': dommages > 0 
+          ? "Vous avez obtenu $jet ! L'attaque réussit et inflige $dommages Points de Dommage avec $armeUtilisee."
+          : "Vous avez obtenu $jet. L'attaque échoue lamentablement..."
+    };
+  }
+}
