0c8cccc832afd287d30e2094ef6895ed732702d2
Matthieu Sublet <msublet@gmail.com>
2026-06-11 23:03:21 +0200

    Update book_models.dart

--- lib/models/book_models.dart
+++ lib/models/book_models.dart
@@ -5,17 +5,12 @@ class GameBook {
   final DreamTime tempsDuReve;
   final List<Chapter> histoire;
 
-  GameBook({
-    required this.annexes,
-    required this.tempsDuReve,
-    required this.histoire,
-  });
+  GameBook({required this.annexes, required this.tempsDuReve, required this.histoire});
 }
 
 class DreamTime {
   final String description;
   final List<Paragraph> sections;
-
   DreamTime({required this.description, required this.sections});
 }
 
@@ -29,13 +24,8 @@ class Chapter {
   final List<Paragraph>? paragraphes;
 
   Chapter({
-    required this.order,
-    required this.title,
-    required this.type,
-    this.contenu,
-    this.suivant,
-    this.paragrapheDepart,
-    this.paragraphes,
+    required this.order, required this.title, required this.type,
+    this.contenu, this.suivant, this.paragrapheDepart, this.paragraphes,
   });
 }
 
@@ -43,22 +33,21 @@ class Paragraph {
   final String id;
   final String text;
   final List<Choice>? choices;
+  final Enemy? enemy; // L'ennemi potentiel de ce paragraphe
 
-  Paragraph({
-    required this.id,
-    required this.text,
-    this.choices,
-  });
+  Paragraph({required this.id, required this.text, this.choices, this.enemy});
 }
 
 class Choice {
   final String text;
   final String nextId;
-  final Map<String, dynamic>? condition;
+  Choice({required this.text, required this.nextId});
+}
 
-  Choice({
-    required this.text,
-    required this.nextId,
-    this.condition,
-  });
+// NOUVEAU : La classe qui définit un monstre
+class Enemy {
+  final String name;
+  final int lifePoints;
+
+  Enemy({required this.name, required this.lifePoints});
 }
